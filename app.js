9b927cc6c8f1fa8f90ad5acc4133b1c83216c953
arefallabadi <labadi.aref@gmail.com>
2021-05-11 16:39:42 +0300

    practice Q5

--- app.js
+++ app.js
@@ -23,3 +23,28 @@ app.get("/todos", (req, res) => {
     todos.push(newT);
     res.json(newT);
   });
+
+  //Q5
+  app.put("/update/todo/:name", (req, res) => {
+    let index;
+    const newT = { todo: req.body.todo, isCompleted: req.body.isCompleted };
+    const found = todos.find((elm, i) => {
+      index = i;
+      return elm.todo === req.params.name;
+    });
+  
+    if (found) {
+      res.status(200);
+      todos[index] = newT;
+      res.json(newT);
+    } else {
+      res.status(404);
+      res.json("Not Found");
+    }
+  });
+  
+
+
+  app.listen(port, () => {
+    console.log(`run on http://localhost:${port}`);
+  });
