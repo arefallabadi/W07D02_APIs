4274d9a0cf8774d6d3d00444eda9e6d4f4e09ebc
arefallabadi <labadi.aref@gmail.com>
2021-05-11 16:25:45 +0300

    practice Q3 && Q4

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ const todos = [
     { todo: "Eat Breakfast", isCompleted: false },
   ];
 app.use(express.json());
+
+//Q3
+app.get("/todos", (req, res) => {
+    res.json(todos);
+  });
+
+  //Q4
+  app.post("/create/todo", (req, res) => {
+    res.status(201);
+    const newT = { todo: req.body.todo, isCompleted: req.body.isCompleted };
+    todos.push(newT);
+    res.json(newT);
+  });
